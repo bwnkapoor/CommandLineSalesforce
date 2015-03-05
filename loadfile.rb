b65309cf87295f22980f22951c49429a1c416b2b
Justin Catterson <justin.catterson@gmail.com>
2015-03-04 21:58:03 -0500

    Pusing and pulling files is possible now

--- loadfile.rb
+++ loadfile.rb
@@ -31,42 +31,31 @@ def pull
 end
 
 def push
-  container = MetadataContainer.new('test', '1dcj0000000gyfB')
-
+  container = MetadataContainer.new( DateTime.now.to_time.to_i.to_s )
+  container.save()
   cls = ApexClass.new()
-  cls.save container
+  cls.load_from_local_file("humbug/TestController2.cls")
+  saving_classes = { cls.name=>cls }
+  puts cls.save( container )
+  puts "saving..."
+  asynch = ContainerAsyncRequest.new( container.id )
+  deploy_id = asynch.save
+  results = nil
+  while !results || (results.State != 'Completed' && results.State != 'Failed')
+    puts "sleeping"
+    sleep(1)
+    results = Salesforce.instance.metadata_query "Select DeployDetails, State from ContainerAsyncRequest where id = \'#{deploy_id}\'"
+    results = results.body.current_page[0]
+  end
 
-  asynch = ContainerAsyncRequest.new('1dcj0000000gyfB')
-  puts "Saving..."
-  puts asynch.save
-  puts "done"
-end
-
-container = MetadataContainer.new( DateTime.now.to_time.to_i.to_s )
-container.save()
-cls = ApexClass.new()
-cls.load_from_local_file("humbug/TestController2.cls")
-saving_classes = { cls.name=>cls }
-puts cls.save( container )
-puts "saving..."
-asynch = ContainerAsyncRequest.new( container.id )
-deploy_id = asynch.save
-puts "done"
-results = nil
-while !results || (results.State != 'Completed' && results.State != 'Failed')
-  puts "sleeping"
-  sleep(1)
-  results = Salesforce.instance.metadata_query "Select DeployDetails, State from ContainerAsyncRequest where id = \'#{deploy_id}\'"
-  results = results.body.current_page[0]
-end
-
-results.DeployDetails.allComponentMessages.each do |message|
-  fileName = message.fileName.to_s
-	if message.success then puts "Success" else puts "Oh No!" end
-	if !message.success
-		puts message.problem.to_s
-		puts message.lineNumber.to_s
-		puts message.problemType.to_s
-		puts saving_classes[fileName].local_name
-	end
+  results.DeployDetails.allComponentMessages.each do |message|
+    fileName = message.fileName.to_s
+    if message.success then puts "Success" else puts "Oh No!" end
+    if !message.success
+      puts message.problem.to_s
+      puts message.lineNumber.to_s
+      puts message.problemType.to_s
+      puts saving_classes[fileName].local_name
+    end
+  end
 end
